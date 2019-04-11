711de7e0481759b782bac180e32c04db587cefea
Abduraqzzak1200 <toloev.abi98@gmail.com>
2019-04-11 15:34:35 +0600

    end slider

--- src/components/Footer/FooterSocial/FooterSocial.js
+++ src/components/Footer/FooterSocial/FooterSocial.js
@@ -13,43 +13,43 @@ const FooterSocial = () => {
     return (
         <div className='social'>
             <h4 className="social__title">
-            КАК СВЯЗАТЬСЯ?
+                КАК СВЯЗАТЬСЯ?
             </h4>
             <ul className="social__list">
                 <li className="social__item">
-                    <a href="/">
-                        <img className='social__img' src={facebook} alt="insta" className='social__img'/>
+                    <a href="http://facebook.ru">
+                        <img className='social__img' src={facebook} alt="insta" className='social__img' />
                     </a>
                 </li>
                 <li className="social__item">
-                <a href="/">
-                    <img className='social__img' src={googlep} alt="google" className='social__img'/>
-                </a>
-                </li>
-                <li className="social__item">
-                    <a href="/">
-                        <img className='social__img' src={insta} alt="insta" className='social__img'/>
+                    <a href="http://facebook.ru">
+                        <img className='social__img' src={googlep} alt="google" className='social__img' />
                     </a>
                 </li>
                 <li className="social__item">
-                    <a href="/">
-                        <img className='social__img' src={linkedin} alt="linkedin" className='social__img'/>
+                    <a href="http://facebook.ru">
+                        <img className='social__img' src={insta} alt="insta" className='social__img' />
                     </a>
                 </li>
                 <li className="social__item">
-                <a href="/">
-                    <img className='social__img' src={pinterest} alt="pinterest" className='social__img'/>
-                </a>
+                    <a href="http://facebook.ru">
+                        <img className='social__img' src={linkedin} alt="linkedin" className='social__img' />
+                    </a>
                 </li>
                 <li className="social__item">
-                <a href="/">
-                    <img className='social__img' src={telegram} alt="telegram" className='social__img'/>
-                </a>
+                    <a href="http://facebook.ru">
+                        <img className='social__img' src={pinterest} alt="pinterest" className='social__img' />
+                    </a>
                 </li>
                 <li className="social__item">
-                <a href="/">
-                    <img className='social__img' src={youtube} alt="youtube" className='social__img'/>
-                </a>
+                    <a href="http://facebook.ru">
+                        <img className='social__img' src={telegram} alt="telegram" className='social__img' />
+                    </a>
+                </li>
+                <li className="social__item">
+                    <a href="http://facebook.ru">
+                        <img className='social__img' src={youtube} alt="youtube" className='social__img' />
+                    </a>
                 </li>
             </ul>
         </div>
